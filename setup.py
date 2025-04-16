dc8430a0e783c706348318f581524c175457737a
Richard Genet <richard.genet@gmail.com>
2025-04-16 11:06:05 -0400

    didn't commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lang-pz-workflow",
+    name="langpz",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "langgraph>=0.1.0",
         "langchain-openai>=0.3.12",
-        "langchain>=0.3.23",
-        "langsmith>=0.3.28",
+        "langchain>=0.3.0",
+        "langsmith>=0.3.0",
         "pydantic>=2.0,<3.0",
         "requests>=2.0.0",
         "multidict==6.0.4",
